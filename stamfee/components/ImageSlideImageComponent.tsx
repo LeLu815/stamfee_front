43e683531538e13c4c0cddd5afaa429fe615dd60
IN <leein9404@naver.com>
2024-03-21 15:08:13 +0900

    feat : 이미지 스크롤 슬라이딩시 자식 인덱스 표시 기능

---
+++ stamfee/components/ImageSlideImageComponent.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useInView } from "react-intersection-observer";
+import Image from "next/image";
+
+interface ImageConfig {
+  // key: number;
+  index: number;
+  url: string;
+  parentElement: HTMLDivElement | null;
+  changeFun: (index: number) => void;
+}
+
+export default function ImageSlideImageComponent(config: ImageConfig) {
+  const [ref, inView, entry] = useInView({
+    root: config.parentElement,
+    threshold: 0.5,
+  });
+
+  // 올려주기 함수를 받아서 inView 값을 줘야 한다.
+  if (inView) {
+    config.changeFun(config.index);
+  }
+
+  return (
+    <Image
+      className="snap-center w-w80 mx-m20 max-h object-cover h-h85 mt-auto"
+      src={config.url}
+      alt="광고 페이지 이미지"
+      ref={ref}
+    />
+  );
+}
--- stamfee/components/imageSlideComponent.tsx
+++ stamfee/components/imageSlideComponent.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+//
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
+import { useInView } from "react-intersection-observer";
+
+// import custom components
+import ImageSlideImageComponent from "./ImageSlideImageComponent";
 
 // svg url
 import introSvg_1 from "@/public/svg/intro_img/intro_1.svg";
@@ -13,26 +18,60 @@ export default function ImageSlideComponent({
 }: {
   imgaeList: string[];
 }) {
-  const [imageNum, setImageNum] = useState(0);
-
+  // imgaeList 값 결정
   const imageListCheck =
     imgaeList.length !== 0 ? imgaeList : [introSvg_1, introSvg_2, introSvg_3];
 
+  const [imageNum, setImageNum] = useState(0);
+  const parentRef = useRef<null | HTMLDivElement>(null);
+
+  const changeImageIndex = useCallback(
+    (index: number) => {
+      setImageNum(index);
+    },
+    [setImageNum]
+  );
+  // const childrenRef = useRef<null[] | HTMLDivElement[]>([]);
+  // const [ref, inView, entry] = useInView({
+  //   root: parentRef.current,
+  //   threshold: 0.5,
+  // });
+
+  // const setRefs = useCallback(
+  //   (node:HTMLDivElement | null, num:number) => {
+  //     childrenRef.current[num] = node;
+  //     ref(node);
+  //   },
+  //   [ref],
+  // );
+
   return (
     <>
-      <div className="w-w80 mx-auto aspect-square overflow-auto overflow-x-scroll snap-x snap-mandatory flex">
+      <div
+        className="w-w80 mx-auto aspect-square overflow-auto overflow-x-scroll snap-x snap-mandatory flex"
+        ref={parentRef}
+      >
         {imageListCheck.map((value, index) => (
-          <Image
-            className="snap-center w-w80 mx-m20 max-h object-cover h-h85 mt-auto"
+          <ImageSlideImageComponent
             key={index}
-            src={value}
-            alt="광고 페이지 이미지"
+            index={index}
+            url={value}
+            parentElement={parentRef.current}
+            changeFun={changeImageIndex}
           />
+          // <Image
+          //   className="snap-center w-w80 mx-m20 max-h object-cover h-h85 mt-auto"
+          //   key={index}
+          //   src={value}
+          //   alt="광고 페이지 이미지"
+          // />
         ))}
       </div>
       <div>
+        {imageNum}
         {imageListCheck.map((value, index) => (
-          <Image src={} />
+          <></>
+          // <Image src={} />
         ))}
       </div>
     </>
